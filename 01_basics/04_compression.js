23161ed3ccf9ed30fc8fc598b3f1c930e50b8d8e
aarymane <121445797+aarymane@users.noreply.github.com>
2023-11-23 12:12:08 +0000

    config compression

--- 01_basics/04_compression.js
+++ 01_basics/04_compression.js
@@ -3,7 +3,10 @@ console.log(2>=1);
 
 console.log("2">1);
 console.log("02">1);
-
+ ///this brings confusion
 console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
+
+/// === strictly checks the value
+ console.log("2"=== 2)
