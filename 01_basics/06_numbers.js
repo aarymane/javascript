c2d6846611ba0af46c92468637aea51250284a86
aarymane <121445797+aarymane@users.noreply.github.com>
2023-11-25 08:39:46 +0000

    num and maths

---
+++ 01_basics/06_numbers.js
@@ -0,0 +1,37 @@
+// const score =400;
+// console.log(score);
+
+// const balance =new Number(100)
+// console.log(balance)//here number is printed with premanent number defination
+
+// console.log(typeof balance.toString());
+// console.log(balance.toString().length);//CONVERT TO STRING
+// console.log(balance.toFixed(3));//IT IS USED TO DISPLAY THE DECIMAL NUMBER
+
+// const numbers=23.67890;
+// console.log(numbers.toPrecision(4))//IT IS USED TO ROUND-OFF THE NUMBERS
+
+// const hundreds=100000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+// //THIS LOCALE-STRING IS USED TO DISPLAY NUMBERS IN FORM OF CURRENCY
+// //EN -(COUNTRY NAME)
+
+// //++++++++++++++++++++++++  MATHS  +++++++++++++++++++++
+// console.log(Math)
+// console.log(Math.abs(-4));
+// console.log(Math.floor(5.6));//niche wali value
+// console.log(Math.ceil(7.9));//upar wali value
+
+
+console.log(Math.random())
+//it give any random value between 0 and 1;
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+//min is added to give number between this range
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
