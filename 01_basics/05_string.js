d88997533aa5b5ea14c5a3986c695bb73233fa6f
aarymane <121445797+aarymane@users.noreply.github.com>
2023-11-25 06:05:27 +0000

    STRINGS

---
+++ 01_basics/05_string.js
@@ -0,0 +1,19 @@
+const name ="aaryesh"
+const repocount=50;
+
+console.log(name + repocount + "value")
+
+console.log(`my name is ${name} and my repocount is 
+${repocount}`)
+
+const gamename=new String('aaryesh-mane')
+console.log(gamename[0])
+console.log(gamename.__proto__)
+
+console.log(gamename.length)
+console.log(gamename.charAt(5))
+console.log(gamename.toUpperCase())
+console.log(gamename.concat())
+
+const newstringh =gamename.substring(0,5)
+console.log(newstringh)
